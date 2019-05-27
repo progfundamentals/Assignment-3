e2c34600551dee62219db0cd391d1c33c04518c2
Felicity R <50560228+felicityR@users.noreply.github.com>
2019-05-27 10:28:58 +1000

    Update rsa.c
    new version, with only required libaries. encrypts string in plain.txt to cipher.txt, then decrypts this string, storing it in decrypt.txt

--- rsa.c
+++ rsa.c
@@ -1,40 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h> /*frekin hell chnage entire code, dont use this whole thing*/
-#include <math.h>
-	int e, d, n; /*move global variables*/
 
-/*finds greatest common divisor of two ints*/
-int gcd(int a, int b)
-{
+  /* The following outlines the process for RSA encryption and decyrption :
+
+   1.Two prime numbers are selected as p and q
+   2. n = pq;  the modulus of both the keys
+   3.the totent is calculated  = (p-1)(q-1)
+   4. Choose e such that e > 1 and gcd (e, totient) must be equal to 1, making e is the public key
+   5. Choose d such that it satisfies the equation de = 1 + k (totient), d is the private key.
+   6. Cipher text is calculated using the equation c = m^e mod n where m is an unencrpted string
+   7. using c and d, the message is decytped using equation m = c^d mod n, d being the private key.*/
+
+
+/*preprocessor directives*/
+#include <stdio.h>    
+#include <stdlib.h>
+#include <math.h>
+
+/*global variables used by most functions*/
+	int e, d, n; 
+
+/*Finds greatest common divisor of two integers,
+ensuring safer encryption*/
+int gcd(int a, int b){
 	int q, r1, r2, r;
 
-	if (a > b) {
-		r1 = a;
-		r2 = b;
+	if (a>b) {
+	r1=a;
+	r2=b;
 	}
 	else {
-		r1 = b;
-		r2 = a;
+	r1=b;
+	r2=a;
 	}
-
-	while (r2 > 0) {
-		q = r1 / r2;
-		r = r1 - q * r2;
-		r1 = r2;
-		r2 = r;
+	while (r2>0) {
+	q = r1/r2;
+	r = r1 - q * r2;
+	r1 = r2;
+	r2 = r;
 	}
-
 	return r1;
 }
 
-
-
-int FastExponention(int bit, int n, int* y, int* a)
-{
+int FastExponention(int bit, int n, int* y, int* a){
 	if (bit == 1)
-		*y = (*y * (*a)) % n;
-	
+	*y = (*y * (*a)) % n;
 	*a = (*a) * (*a) % n;
 }
 /*finds the carmicheal totent*/
@@ -50,21 +58,19 @@ int FindT(int a, int m, int n)
 	}
 	return y;
 }
-/*this function efficently tests if the two prime numbers
+/*this function tests if the two prime numbers selected
 are in fact primary numbers*/
-int PrimarityTest(int a, int i)
-{
+int PrimarityTest(int a, int i){
 	int n = i - 1;
 	int k = 0;
 	int j, m, T;
 
-	while (n % 2 == 0) {
+	while (n % 2 == 0){
 		k++;
 		n = n / 2;
 	}
-
 	m = n;
-	T = FindT(a, m, i);
+	T = FindT(a, m,i);
 
 	if (T == 1 || T == i - 1)
 		return 1;
@@ -76,11 +82,12 @@ int PrimarityTest(int a, int i)
 		if (T == i - 1)
 			return 1;
 	}
+
 	return 0;
 } 
 
-int inverse(int a, int b)
-{
+int inverse(int a, int b){
+
 	int inv;
 	int q, r, r1 = a, r2 = b, t, t1 = 0, t2 = 1;
 
@@ -89,7 +96,6 @@ int inverse(int a, int b)
 		r = r1 - q * r2;
 		r1 = r2;
 		r2 = r;
-
 		t = t1 - q * t2;
 		t1 = t2;
 		t2 = t;
@@ -100,109 +106,110 @@ int inverse(int a, int b)
 
 	if (inv < 0)
 		inv = inv + a;
-
 	return inv;
 }
-
+/*finds the encyption keys, randomly selects 2 prime integers */
 int KeyGeneration()
 {
-	int p, q;
+	int q,p; 
 	int phi_n;
 	
+do {
+	do
+	/*picks random int for p, tests whether it is a prime*/
+		p = rand(); 
+    while (p % 2 == 0);
+
+} while (!PrimarityTest(2, p));
+
+/*picks random integer for q, then tests it*/
 	do {
 		do
-			p = rand(); /*picks random int*/
-		while (p % 2 == 0);
-
-	} while (!PrimarityTest(2, p));
-
-	do {
-		do
-			q = rand(); /*picks random int*/
+			q = rand(); 
 		while (q % 2 == 0);
 	} while (!PrimarityTest(2, q));
-
+/*modulus*/
 	n = p * q;
+	/*totent */
 	phi_n = (p - 1) * (q - 1);
 
 	do
-		e = rand() % (phi_n - 2) + 2; // 1 < e < phi_n
+	/*ensures the gcd(e,totent) is equal to 1, then finds e accordingly*/
+		e = rand() % (phi_n - 2) + 2; /*1 < e < phi_n*/
 	while (gcd(e, phi_n) != 1);
 
+/*d is the private key*/
 	d = inverse(phi_n, e);
 }
 
+/*encryption code */
 int Encryption(int value, FILE* out)
 {
-	int cipher;
-	cipher = FindT(value, e, n);
-	fprintf(out, "%d ", cipher);
+	int ciph;
+	ciph= FindT(value, e, n);
+	fprintf(out, "%d ", ciph);
 }
-
+/*decyrption code*/
 int Decryption(int value, FILE* out)
 {
-	int decipher;
-	decipher = FindT(value, d, n);
-	fprintf(out, "%c", decipher);
+	int deciph;
+	deciph = FindT(value, d,n);
+	fprintf(out,"%c",deciph);
 }
-
 int main(void)
 {
-
 	FILE *inp, *out;
 
 	// destroy contents of these files (from previous runs, if any)
-	out = fopen("cipher.txt", "w+");
-	fclose(out);
-	out = fopen("decipher.txt", "w+");
-	fclose(out);
+out=fopen("cipher.txt","w+");
+fclose(out);
+out=fopen("decipher.txt","w+");
+fclose(out);
 
 	KeyGeneration();
 
-	inp = fopen("plain.txt", "r+");
+	inp=fopen("plain.txt","r+");
 	if (inp == NULL) {
-		printf("Error opening Source File.\n");
+		printf("error accessing source file.\n");
 		exit(1);
 	}
 
-	out = fopen("cipher.txt", "w+");
+	out=fopen("cipher.txt","w+");
 	if (out == NULL) {
 		printf("Error opening Destination File.\n");
 		exit(1);
 	}
 
-	// encryption starts
+	/*encryption starts here*/
 	while (1) {
 		char ch = getc(inp);
 		if (ch == -1)
 			break;
-		int value = toascii(ch);
+	/*changes value to ascii*/
+		int value = ch;                         
 		Encryption(value, out);
 	}
-
 	fclose(inp);
 	fclose(out);
 
-	// decryption starts
-	inp = fopen("cipher.txt", "r");
+	/*decrpytion starts here*/
+	inp = fopen("cipher.txt","r");
 	if (inp == NULL) {
-		printf("Error opening Cipher Text.\n");
+		printf("error opening cipher file\n");
 		exit(1);
 	}
-
-	out = fopen("decipher.txt", "w+");
+	out = fopen("decipher.txt","w+");
 	if (out == NULL) {
-		printf("Error opening File.\n");
+		printf("error opening files\n");
 		exit(1);
 	}
-
+/*activates decryption code with new input*/
 	while (1) {
 		int cip;
-		if (fscanf(inp, "%d", &cip) == -1)
+		if (fscanf(inp,"%d",&cip) == -1)
 			break;
 		Decryption(cip, out);
 	}
 	fclose(out);
-
 	return 0;
 }
